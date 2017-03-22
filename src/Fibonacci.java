7f3b79ca312f1fddbb9fc5c90a1b88b73fb41c99
Natalia Mocanu <nmocanu@endava.com>
2017-03-22 10:44:59 +0200

    Initial commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,19 @@
+
+public class Fibonacci {
+
+    public static long fib(long n){
+        if (n==0)
+            return 0;
+        else if (n==1)
+            return 1;
+        else
+          return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args) {
+
+        System.out.println(fib(20));
+
+    }
+
+
+}
